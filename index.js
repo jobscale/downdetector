8fc2017f1f43a0835ab576b0ce85cb39f22ded3b
jobscale <jobscale@jsx.jp>
2019-05-26 20:43:12 +0900

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+require('modules');
+const _ = require('lodash');
+const { JSDOM } = require('jsdom');
+
+class DownDetector {
+  run() {
+    const urls = [{
+      location: 'jp',
+      url: 'https://downdetector.jp/top10',
+    }, {
+      location: 'com',
+      url: 'https://downdetector.com/top10',
+    }];
+    return Promise.all(
+      urls.map(url => this.parse(url)),
+    );
+  }
+  parse(param) {
+    return fetch(param.url)
+    .then(res => res.text())
+    .then(body => new JSDOM(body).window.document)
+    .then(document => {
+      const el = document.querySelector('table');
+      const list = _.values(document.querySelectorAll('table td:nth-child(5) a'));
+      list.length = 3;
+      return {
+        body: el.innerHTML,
+        caption: `${param.location} - ${list.map(cel => cel.textContent.trim()).join(', ')}`,
+        image: el.querySelector('img').src,
+      };
+    });
+  }
+}
+module.exports = {
+  DownDetector,
+};
